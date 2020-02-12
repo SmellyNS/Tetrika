6d5aaa5cb717f1914bf9aec0fb4982eac209dc8d
SmellyNS <dmitryl5311@ya.ru>
2020-02-12 17:15:35 +0300

    DBalter.sql fix

--- 3/DBalter.sql
+++ 3/DBalter.sql
@@ -1,11 +1,8 @@
 use tetrika
 go
 
-
 create table #tmp (event_id int, avg_qty float)			-- temporary table for saving (event, avg) table
 GO
-
-
 insert #tmp (event_id, avg_qty)		
 			select event_id, avg(tech_quality) as avg_qty
 			from (
@@ -41,17 +38,26 @@ insert #tmp (event_id, avg_qty)
 
 
 -- joining #tmp -> lessons -> participants -> users to get required data
-select cast(scheduled_time as date) as scheduled_time, user_id, avg_qty from 
+
+
+	SELECT scheduled_time, user_id, avg_qty
+	from (
+		SELECT tmpid, scheduled_time, user_id, avg_qty, 
+		ROW_NUMBER() OVER
+			(	
+				PARTITION BY scheduled_time, user_id, avg_qty
+				ORDER BY tmpid
+			) as uniqchk
+		FROM 
+
+		(
+		select row_number() over(order by(scheduled_time)) as tmpid, scheduled_time, user_id, avg_qty from
+(select C.event_id, avg_qty, min_avg, user_id, scheduled_time from
 (
-select scheduled_time, user_id, avg_qty from
-(
-	(select A.event_id, avg_qty, scheduled_time from
-		(select event_id, avg_qty from #tmp where avg_qty = (select min(avg_qty) from #tmp)) A
-		join
-		(select event_id, dateadd(hour,3,scheduled_time) as scheduled_time from lessons) B on A.event_id = B.event_id
-	) C
-	join
-	(select event_id, user_id from participants) D on C.event_id = D.event_id ) ) E
-		join
-	(select * from users where role='tutor') F on F.id = E.user_id
+select A.event_id, avg_qty, id, cast(scheduled_time as date) as scheduled_time, min(avg_qty) over (partition by cast(scheduled_time as date)) as min_avg  from ( (select * from #tmp) A join (select * from lessons) B on A.event_id = B.event_id)
+) C join (select * from participants) D on C.event_id = D.event_id ) E JOIN (select * from users where role = 'tutor') F on E.user_id = F.id
+where min_avg = avg_qty ) as L
+
+		) as A
+WHERE A.uniqchk = 1
 
